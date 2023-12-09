c015161f8355951fc17401a393186dd1963dcaa1
bognaradam26 <bognaradam26@gmail.com>
2023-12-09 19:04:25 +0100

    FirestoreCollection and Document models

---
+++ Models/FirestoreCollection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace FirebaseBackupWindowsForm.Models
+{
+    internal class FirestoreCollection
+    {
+        public string? Id { get; set; }
+        public List<FirestoreDocument>? Documents { get; set; }
+    }
+}
---
+++ Models/FirestoreDocument.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+
+namespace FirebaseBackupWindowsForm.Models
+{
+    internal class FirestoreDocument
+    {
+        [JsonPropertyName("Data")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public Dictionary<string, object>? Data { get; set; }
+
+        [JsonPropertyName("Subcollections")]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public List<FirestoreCollection>? Subcollections { get; set; }
+    }
+}
